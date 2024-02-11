239077d253f7a4453055375028eb90e17a6f39a5
Carey99 <edwinakidah1@gmail.com>
2024-02-11 02:04:46 -0800

    Hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,30 +7,40 @@ import os
 
 
 class FileStorage():
-    __file_path = "file.json"
-    __objects = {}
+    __file_path: str = "file.json"
+    __objects: dict = {}
+
+    def __init__(self):
+        """constructor"""
+        pass
 
     def all(self):
         """Returns the dictionary __objects."""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key..."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes to json file"""
-        with open(self.__file_path, 'w') as f:
-            json.dump({key: obj.to_dict() for key, obj in self.__objects.items()}, f)
+        serialized = {
+            key: val.to_dict()
+            for key, val in self.__objects.items()
+        }
+        with open(FileStorage.__file_path, "w") as f:
+            f.write(json.dumps(serialized))
 
     def reload(self):
         """deserializes json files"""
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    class_name, obj_id = key.split('.')
-                    module = __import__('models.' + class_name, fromlist=[class_name])
-                    class_ = getattr(module, class_name)
-                    self.__objects[key] = class_(**value)
+        try:
+            deserialized = {}
+            with open(FileStorage.__file_path, "r") as f:
+                deserialized = json.loads(f.read())
+            FileStorage.__objects = {
+                key:
+                    eval(obj["__class__"])(**obj)
+                    for key, obj in deserialized.items()}
+        except (FileNotFoundError):
+            pass
