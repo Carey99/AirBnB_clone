582174f651a6ccec543a12f65ebde6a95d951379
Carey99 <edwinakidah1@gmail.com>
2024-02-11 04:18:31 -0800

    The console

--- console.py
+++ console.py
@@ -6,26 +6,22 @@ import sys
 import cmd
 
 
-class Myconsole(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     """Class inheriting from cmd"""
     prompt = "(hbnb) "
 
     def do_EOF(self, arg):
         """Exit the console"""
-        print()
         return True
 
     def do_quit(self, arg):
         """Exits gracefully"""
         return True
 
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
 
 if __name__ == "__main__":
-    my_console = Myconsole(stdin=sys.stdin, stdout=sys.stdout)
-
-    if len(sys.argv) > 1:
-        with open(sys.argv[1], 'r') as file:
-            line = file.readlines()
-        my_console.onecmd('\n'.join(lines))
-    else:
-        my_console.cmdloop()
+    HBNBCommand().cmdloop()
